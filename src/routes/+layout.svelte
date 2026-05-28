69b432cd2de80f1e4dea46d0a94f8010ee362eee
TrueSunGaming <truesungaming@gmail.com>
2026-05-27 20:33:06 -0400

    feat: initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import "$lib/style/fonts.scss";
+    import "$lib/style/colors.scss";
+    import "$lib/style/global.scss";
+    import "$lib/style/classes.scss";
+
+    const { children } = $props();
+</script>
+
+<main>
+    {@render children()}
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+Hello 
+<strong>Hello</strong>
+<h1>Hello</h1>
+
+<textarea class="round primary-dark"></textarea>
+<textarea class="round secondary"></textarea>
+
+<section class="round primary-dark">
+    <button class="round hover secondary">Hello</button>
+</section>
