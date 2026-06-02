9a7430667c40f712cacaa4eff2ebcc0d4501105c
TrueSunGaming <truesungaming@gmail.com>
2026-06-02 19:37:39 -0400

    refactor: cancel DOM updates instead of moving caret when typing rich text

--- src/lib/document/block/text/TextBlock.svelte
+++ src/lib/document/block/text/TextBlock.svelte
@@ -2,10 +2,9 @@
     import type { BlockComponentProps } from "../BlockComponent";
     import RichText from "../../../richtext/RichText.svelte";
     import { TextBlockData } from "./TextBlockData";
-    import { getActiveTextNode } from "$lib/util/getActiveTextNode";
     import { getCaretPos } from "$lib/util/caret/getCaretPos";
 
-    const { blockData, doc }: BlockComponentProps = $props();
+    const { blockData }: BlockComponentProps = $props();
 
     const text = $derived(blockData instanceof TextBlockData ? blockData.text : undefined);
 
@@ -19,20 +18,17 @@
     });
 
     let beforeCaretPos: number;
-    let beforeText: string;
 </script>
 
 {#if $text !== undefined}
     <p
         contenteditable
         onbeforeinput={() => {
-            beforeText = getActiveTextNode()?.textContent ?? "";
-
             const pos = getCaretPos();
             if (pos === null) throw new Error("Failed to get caret position");
             beforeCaretPos = pos;
         }}
-        oninput={() => (pendingUpdate = richText?.generateNewRaw(beforeCaretPos, beforeText))}
+        oninput={() => (pendingUpdate = richText?.generateNewRaw(beforeCaretPos))}
     >
         <RichText bind:raw={$text} bind:this={richText} />
     </p>
--- src/lib/richtext/RichText.svelte
+++ src/lib/richtext/RichText.svelte
@@ -5,15 +5,28 @@
     import { findCharInStructure } from "./findCharInStructure";
     import { getActiveTextNode } from "$lib/util/getActiveTextNode";
     import { setCaretPos } from "$lib/util/caret/setCaretPos";
+    import { untrack } from "svelte";
 
     interface Props {
         raw: string;
     }
 
     let { raw = $bindable() }: Props = $props();
-    const structure = $derived(richTextToStructure(raw));
 
-    export function generateNewRaw(caretPos: number, beforeText: string): (() => void) | undefined {
+    let cancelUpdates = 0;
+    let renderingRaw = $state("");
+    const structure = $derived(richTextToStructure(renderingRaw));
+
+    $effect(() => {
+        if (cancelUpdates > 0) {
+            cancelUpdates--;
+            return;
+        }
+
+        renderingRaw = raw;
+    });
+
+    export function generateNewRaw(caretPos: number): (() => void) | undefined {
         const caret = findCharInStructure(structure, caretPos);
         if (!caret) return;
         console.log(caret, caret[0].children[caret[1]]);
@@ -22,13 +35,10 @@
         if (!activeTextNode) return;
 
         // svelte-check thinks this is string | null but its just string
-        const afterText = activeTextNode.textContent!;
-        caret[0].children[caret[1]] = afterText;
+        caret[0].children[caret[1]] = activeTextNode.textContent!;
 
+        cancelUpdates++;
         raw = stringifyRichTextStructure(structure);
-
-        const newPos = caretPos + afterText.length - beforeText.length;
-        return () => setCaretPos(newPos); // so this runs AFTER svelte updates the DOM
     }
 </script>
 
