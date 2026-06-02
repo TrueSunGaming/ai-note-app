1bc900ccef8c58ab7bcc8e06d2a8071eeea6e3a3
TrueSunGaming <truesungaming@gmail.com>
2026-06-02 19:16:22 -0400

    fix: eslint *.svelte.ts files

--- eslint.config.js
+++ eslint.config.js
@@ -53,7 +53,7 @@ export default ts.config(
         }
     },
     {
-        files: ["**/*.svelte"],
+        files: ["**/*.svelte", "**/*.svelte.ts"],
 
         languageOptions: {
             parserOptions: {
