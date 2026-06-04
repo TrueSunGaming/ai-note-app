6f44c1ed168231c0711fb15ea9a87d57d2e4160e
TrueSunGaming <truesungaming@gmail.com>
2026-06-04 17:48:01 -0400

    feat: implement deletePath

---
+++ src/lib/richtext/path/deletePath.ts
@@ -0,0 +1,6 @@
+import type { RichTextStructure } from "../RichTextStructure";
+import { setTextAtPath } from "./setTextAtPath";
+
+export function deletePath(structure: RichTextStructure, path: string): void {
+    setTextAtPath(structure, path, "");
+}
